529e690f4e93a28105069b7f5750237e4b9f69d6
joshbiggins <47434316+joshbiggins@users.noreply.github.com>
2020-02-06 20:57:53 -0600

    Add files via upload

---
+++ Assets/ArmEnd.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmEnd : MonoBehaviour {
+
+    public byte numberOfPieces; //Number of pieces in limb
+    public Transform armStart; //Start piece (transform) of limb
+    public GameObject armPiece; //Prefab for middle pieces of limb
+    public float bend; //Amount limb should bend outwards
+
+    private Vector3 target;
+
+    // Use this for initialization
+    void Start () {
+        target = transform.position;
+        for(byte i = 1; i <= numberOfPieces; i++)
+        {
+            ArmPiece piece = GameObject.Instantiate(armPiece).GetComponent<ArmPiece>();
+            piece.armEnd = transform;
+            piece.armStart = armStart;
+            piece.position = i;
+            piece.Init();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButton(0))
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+            transform.position = target;
+        }
+	}
+
+}
---
+++ Assets/ArmPiece.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmPiece : MonoBehaviour {
+    public byte position; //Position in line
+    public Transform armEnd; //End piece of limb
+    public Transform armStart; //Start piece of limb
+    public bool fixedLimb; //Is limb fixed?
+
+    private float bend;
+    private ArmEnd armEndScript;
+    private byte numberOfPieces;
+    private Vector3 originalVector;
+
+	// Use this for initialization
+	public void Init () {
+        armEndScript = armEnd.gameObject.GetComponent<ArmEnd>();
+        numberOfPieces = armEndScript.numberOfPieces;
+        originalVector = armEnd.position - armStart.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (numberOfPieces > 0)
+        {
+            bend = armEndScript.bend;
+            Vector3 armVector = armEnd.position - armStart.position;
+            
+            if(fixedLimb)
+            {
+                transform.position = Vector3.Lerp(armStart.position, armStart.position + originalVector, (float)position / numberOfPieces * 0.4f);
+                transform.position = Vector3.Lerp(transform.position, armEnd.position, (float)position / numberOfPieces * 0.8f);
+            }
+            if (!fixedLimb)
+            {
+                Vector3 perpVector = new Vector3(armVector.y, armVector.x, 0.0f).normalized * bend;
+
+                float distFromMiddle = Mathf.Abs((float)position - Mathf.Ceil((float)numberOfPieces / 2));
+                if (distFromMiddle < 1)
+                {
+                    distFromMiddle = 0.9f;
+                }
+
+                perpVector = perpVector / distFromMiddle;
+
+                transform.position = Vector3.Lerp(armStart.position, armEnd.position, (float)position / numberOfPieces * 0.8f);
+                transform.position += perpVector;
+            }
+        }
+	}
+}
